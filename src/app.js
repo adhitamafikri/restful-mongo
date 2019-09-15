7ae92d5d3db114b9880a82c9691868676cc9ab4e
adhitamafikri <adhitamafikri@gmail.com>
2019-09-15 19:54:17 +0700

    added model for Posts, added middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 import 'dotenv/config'
 
 import routes from '@routes'
@@ -9,6 +10,7 @@ import { ConnectDB } from '@database/connection'
 const router = express()
 
 // Middlewares
+router.use(bodyParser.json())
 
 // Routes
 routes(router)
